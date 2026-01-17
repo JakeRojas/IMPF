902e5fa56e8ff92cfc8c908c402da0564f5c9daa
Jake Rojas <jakerojas2211@gmail.com>
2026-01-18 02:19:12 +0800

    13, fixing the refresh/reload page issue

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -2,7 +2,7 @@
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { map, finalize, catchError } from 'rxjs/operators';
+import { map, finalize, catchError, share } from 'rxjs/operators';
 
 import { environment } from '@environments/environment';
 import { Account } from '@app/_models';
@@ -13,6 +13,7 @@ const baseUrl = `${environment.apiUrl}/accounts`;
 export class AccountService {
     private accountSubject: BehaviorSubject<Account | null>;
     public account: Observable<Account | null>;
+    private refreshTokenSubject: Observable<any> | null = null;
 
     constructor(
         private router: Router,
@@ -49,13 +50,25 @@ export class AccountService {
         this.router.navigate(['/account/login']);
     }
     refreshToken() {
-        return this.http.post<any>(`${baseUrl}/refresh-token`, {}, { withCredentials: true })
-            .pipe(map((account) => {
-                localStorage.setItem('account', JSON.stringify(account));
-                this.accountSubject.next(account);
-                this.startRefreshTokenTimer();
-                return account;
-            }));
+        if (this.refreshTokenSubject) {
+            return this.refreshTokenSubject;
+        }
+
+        this.refreshTokenSubject = this.http.post<any>(`${baseUrl}/refresh-token`, {}, { withCredentials: true })
+            .pipe(
+                map((account) => {
+                    localStorage.setItem('account', JSON.stringify(account));
+                    this.accountSubject.next(account);
+                    this.startRefreshTokenTimer();
+                    return account;
+                }),
+                finalize(() => {
+                    this.refreshTokenSubject = null;
+                }),
+                share()
+            );
+
+        return this.refreshTokenSubject;
     }
     revokeToken() {
         const refreshToken = localStorage.getItem('refreshToken');
@@ -123,12 +136,18 @@ export class AccountService {
     // helper methods
     private refreshTokenTimeout?: any;
     private startRefreshTokenTimer() {
-        const jwtBase64 = this.accountValue!.jwtToken!.split('.')[1];
+        if (!this.accountValue?.jwtToken) return;
+
+        // parse jwt token to get expiration date
+        const jwtBase64 = this.accountValue.jwtToken.split('.')[1];
         const jwtToken = JSON.parse(atob(jwtBase64));
 
+        // set a timeout to refresh the token a minute before it expires
         const expires = new Date(jwtToken.exp * 1000);
         const timeout = expires.getTime() - Date.now() - (60 * 1000);
-        this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
+        if (timeout > 0) {
+            this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
+        }
     }
     private stopRefreshTokenTimer() {
         clearTimeout(this.refreshTokenTimeout);
